5dca935e23c4d5c5095085d35db1617843eeb586
Isaiah <ijshd7@mail.missouri.edu>
2019-05-20 17:00:32 -0500

    adding start component and css file

--- galactic_react/src/components/Start.js
+++ galactic_react/src/components/Start.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import './Start.css';
 
 export default class Start extends Component {
   render() {
     return (
       <div className="start">
         { this.props.children }
+        <button className="startButton">
+          S T A R T
+        </button>
       </div>
     )
   }
