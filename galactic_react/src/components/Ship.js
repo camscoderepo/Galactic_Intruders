f1ec00011349161863ae6f42201b6750d364eb7e
Jonathan Rowell <rowell.jonathan@yahoo.com>
2019-05-21 21:30:54 -0500

    started ship component

--- galactic_react/src/components/Ship.js
+++ galactic_react/src/components/Ship.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './Ship.css'
 
 export default class Ship extends Component {
   render() {
     return (
-      <div className="ship">
-        { this.props.children }
+      <div className="container">
+        <div className="arrow-up"></div>
       </div>
     )
   }
