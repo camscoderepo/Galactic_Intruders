b1425e0086b08aebcaa3784a0693936a1285256a
Cameron Roman <camroman95@gmail.com>
2019-06-02 10:04:09 -0500

    Changed some css and made the ship a sc

--- galactic_react/src/components/Button.jsx
+++ galactic_react/src/components/Button.jsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 
-const Button = styled.button
-`
+const Button = styled.button`
   position: absolute;
-  color: brown;
-  padding: 15px 32px;
+  padding: 0.25em 1em;
+  margin: 1em;
   text-align: center;
+  border-radius: 3px;
   display: inline-block;
   font-size: 16px;
   cursor: pointer;
   border: solid 5px white;
-  z-index: 2;
+  z-index: 1;
   left: 921px;
   top: 70%;
 `
